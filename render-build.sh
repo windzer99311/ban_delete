9c47543cc10948ed68993c1042490475145df735
windzer99311 <windzergamer@gmail.com>
2025-06-24 17:24:24 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
-set -e
+# exit on errorset -o errexit
 
-echo "📦 Installing packages..."
 npm install
+# npm run build # uncomment if required
 
-echo "🌐 Installing Chromium..."
-npx puppeteer browsers install chrome
+# Store/pull Puppeteer cache with build cache
+if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
+  echo "...Copying Puppeteer Cache from Build Cache" 
+  cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
+else 
+  echo "...Storing Puppeteer Cache in Build Cache" 
+  cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
+fi
